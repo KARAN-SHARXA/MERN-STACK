9ee297d07b6764f48db6dc4ab6484bba3811d4c7
KARAN-SHARXA <karansharma2302578@gmail.com>
2026-01-03 23:23:02 +0530

    loops

---
+++ Phase2/04js.js
@@ -0,0 +1,46 @@
+// condition
+
+
+let age = 20;
+
+if (age >= 18) {
+  console.log("Adult");
+}
+
+
+
+let age1 = 16;
+
+if (age1 >= 18) {
+  console.log("Adult");
+} else {
+  console.log("Minor");
+}
+
+
+
+let day = 3;
+
+switch(day){
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 3:
+    console.log("wednesday");
+}
+
+
+
+
+let re = 20;
+let result = re>=18 ? "Adulc" : "Minor";
+
+
+
+if ("hello") {
+  console.log("This runs");
+}
+
