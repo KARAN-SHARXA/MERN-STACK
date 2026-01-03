7466d2f576aa8cd1cebf07c829c52faa843dcd9b
KARAN-SHARXA <karansharma2302578@gmail.com>
2026-01-03 22:08:43 +0530

    operator

---
+++ Phase2/03js.js
@@ -0,0 +1,46 @@
+/////operators = Operators are symbols that perform operations on values (operands).
+
+
+// Arithmetic
+let a = 5;
+let b = 6;
+console.log(a+b);
+console.log(a-b);
+
+let c = "ka"
+let d = "ra"
+console.log(c+d);
+
+
+console.log(2**4);
+
+
+// comparison =,==,===,!=,!==,>=,<=
+
+console.log(12 == 13);
+
+console.log(12 == "12");
+
+
+// not strict ==
+// === strict comparision
+
+
+let p = 16;
+p++;
+console.log(p);
+
+let x = 5;
+console.log(++x);
+
+
+
+
+// Quick Memory Trick
+
+// ++x → Increase first, then use
+
+// x++ → Use first, then increase
+
+
+
