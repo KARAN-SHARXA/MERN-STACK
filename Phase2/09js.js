b3440f48d78a71868be75081c3c76af79fbe911f
Karan Sharma <karansharma2302578@gmail.com>
2026-01-04 22:02:58 +0530

    object in js

--- Phase2/09js.js
+++ Phase2/09js.js
@@ -10,10 +10,46 @@ console.log("great");
 
 
 
-//new object
-console.log("hi ");
-console.log("rah");
-console.log("hu");
+console.log(person.age);
+person.age = 23;
+person.country = "india";
+console.log(person);
+
+
+let p = {
+  name:"Amit",
+  greet:function(){
+    console.log("hello"+this.name);
+    
+  }
+}
+
+p.greet()
+
+
+let student = {
+  name: "Rahul",
+  marks: {
+    math: 80,
+    science: 75
+  }
+};
+
+console.log(student.marks.math); // 80
+
+
+
+
+
+let user = {
+  name: "Karan",
+  age: 22,
+  city: "Delhi"
+};
+
+for (let key in user) {
+  console.log(key, user[key]);
+}
 
 
 
